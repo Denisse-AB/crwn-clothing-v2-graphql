d14d923b5acc701d906646bcd937e0cbc3971d20
Denisse Abreu <denisseabreu21@yahoo.com>
2022-05-02 20:54:31 -0400

    adding graphql to components

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { gql, useQuery } from '@apollo/client';
 
 export const CategoriesContext = createContext({
@@ -24,11 +24,20 @@ export const CategoriesProvider = ({ children }) => {
   const { loading, error, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  console.log(loading);
-  console.log(data);
-  console.log(error);
-  
-  const value = { categoriesMap };
+ useEffect(() => {
+    if (data) {
+      const { collections } = data;
+      const collectionsMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {});
+
+      setCategoriesMap(collectionsMap);
+    }
+ }, [data])
+
+  const value = { categoriesMap, loading };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
